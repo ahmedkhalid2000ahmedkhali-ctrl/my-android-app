8e05dbb3e86d7dc9be0680e82e67b77217929b2f
ahmedkhalid2000ahmedkhali-ctrl <ahmedkhalid2000ahmedkhali@gmail.com>
2026-09-18 02:33:14 +0300

    Add Capacitor config

---
+++ capacitor.config.ts
@@ -0,0 +1,10 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'com.sda.zadelroh',
+  appName: 'زاد الروح',
+  webDir: 'dist',
+  bundledWebRuntime: false,
+};
+
+export default config;
