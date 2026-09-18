0e4d141dbb1d6f492d7d2f7eaf1993e258cd42f1
ahmedkhalid2000ahmedkhali-ctrl <ahmedkhalid2000ahmedkhali@gmail.com>
2026-09-18 03:09:10 +0300

    Add signOutFirebase function to firebaseClient
    Added signOutFirebase function to handle user sign-out.

--- services/firebaseClient.ts
+++ services/firebaseClient.ts
@@ -1,5 +1,5 @@
 import { initializeApp } from "firebase/app";
-import { getAuth } from "firebase/auth";
+import { getAuth, signOut } from "firebase/auth";
 import { getFirestore } from "firebase/firestore";
 import { getStorage } from "firebase/storage";
 
@@ -17,5 +17,9 @@ const app = initializeApp(firebaseConfig);
 export const auth = getAuth(app);
 export const db = getFirestore(app);
 export const storage = getStorage(app);
+
+export const signOutFirebase = async (): Promise<void> => {
+  await signOut(auth);
+};
 
 export default app;
